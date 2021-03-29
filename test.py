bfd5d3026aadcded3d1fb29c122a337c1e20f563
jannuary <27908024+jannuary@users.noreply.github.com>
2021-03-29 20:52:50 +0700

    added thumbnail downloading

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ from __future__ import unicode_literals
 from ytmusicapi import YTMusic
 import youtube_dl
 import logging
+import wget
+
+
 song_name = input('Enter song name ')
 ytmusic = YTMusic('headers_auth.json')
 search_results = ytmusic.search(song_name, 'songs')
-
 
 # List Songs
 
@@ -16,13 +18,19 @@ for i in range(10):
 # Select a song from the list
 selectedSong = search_results[int(input('Select a song to download.')) - 1]
 
-# Downloader
-ydl_opts = {
-     'format' : 'bestaudio',
-     'outtmpl' : selectedSong['artists'][0]['name'] + ' - ' + selectedSong['title'] + '.m4a',
-     'quiet' : 'true'
-     }
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-     ydl.download([selectedSong['videoId']])
-print('✅ Downloaded \033[95m\033[1m', selectedSong['title'], '\033[0m.')
 
+# # Downloader
+# ydl_opts = {
+#      'format' : 'bestaudio',
+#      'outtmpl' : selectedSong['artists'][0]['name'] + ' - ' + selectedSong['title'] + '.m4a',
+#      'quiet' : 'true'
+#      }
+# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#      ydl.download([selectedSong['videoId']])
+# print('✅ Downloaded \033[95m\033[1m', selectedSong['title'], '\033[0m')
+
+# Thumbnail Grabber
+
+thumbnailURL = selectedSong['thumbnails'][0]['url'].split('=')[0]
+print(thumbnailURL)
+wget.download(thumbnailURL, 'thumbnail.jpg')
